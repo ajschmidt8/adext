0d0cf5267c47904bde2d43428e05310e826d943b
AJ Schmidt <ajs4392@gmail.com>
2020-06-13 09:27:46 -0400

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="adext",
-    version="0.2",
+    version="0.3",
     description="AlarmDecoder extended",
     url="https://github.com/ajschmidt8/adext",
     author="AJ Schmidt",
